de4aec4581c7e4310c29d003185722d785959af6
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2022-05-22 06:14:59 +0100

    Implement more project TODOs

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -1,5 +1,6 @@
+from distutils.log import error
 import json
-from flask import request, _request_ctx_stack
+from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
@@ -90,17 +91,20 @@ def check_permissions(permission, payload):
 
     try:
         if 'permissions' not in payload:
+        #If payload does not contain permissions raise an error.
             raise AuthError({
                 'code': 'permissions_missing',
                 'description': '"permissions" is expected in payload.'
             }, 400)
         
         if permission not in payload['permissions']:
+        #If the specified permission is not in payload permissions, raise an error
             raise AuthError({
                 'code': 'invalid_permissions',
                 'description': 'Payload permissions must include "permission"'
             }, 400)
         
+        #If everytihng is okay, return True
         return True
 
     except:
@@ -120,7 +124,65 @@ def check_permissions(permission, payload):
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+
+    try:
+        jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+        jwks = json.loads(jsonurl.read())
+        unverified_header = jwt.get_unverified_header(token)
+
+        rsa_key = {}
+
+        if 'kid' not in unverified_header:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Authorization malformed'
+            }, 401)
+        
+        for key in jwks['keys']:
+            if key['kid'] == unverified_header['kid']:
+                rsa_key = {
+                    'kty': key['kty'],
+                    'kid': key['kid'],
+                    'use': key['use'],
+                    'n': key['n'],
+                    'e': key['e']
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer='https://' + AUTH0_DOMAIN + '/'
+                )
+                return payload
+
+            except jwt.ExpiredSignatureError:
+                raise AuthError({
+                    'code': 'token_expired',
+                    'description': 'Token has expired.'
+                }, 401)
+
+            except jwt.JWTClaimsError:
+                raise AuthError({
+                    'code': 'invalid_claims',
+                    'description': 'Incorrect claims. Please check the audience and issuer.'
+                }, 401)
+
+            except Exception:
+                raise AuthError({
+                    'code': 'invalid_header',
+                    'description': 'Invalid header. Unable to parse authentication token.'
+                }, 400)
+
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Invalid header. Unable to find appropriate key.'
+        }, 400)
+
+    except:
+        raise Exception('Not Implemented')
 
 '''
 @TODO implement @requires_auth(permission) decorator method
@@ -137,8 +199,14 @@ def requires_auth(permission=''):
         @wraps(f)
         def wrapper(*args, **kwargs):
             token = get_token_auth_header()
-            payload = verify_decode_jwt(token)
+            try: 
+                payload = verify_decode_jwt(token)
+            except error:
+                print(error)
+                abort(401)
+
             check_permissions(permission, payload)
+
             return f(payload, *args, **kwargs)
 
         return wrapper
