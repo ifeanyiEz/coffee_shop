5b59d42d1cf0820b2a43a5fb2ddf0699d5e0bf42
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2022-05-31 03:07:21 +0100

    Edit environment variable

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'ezufsnd.us.auth0.com', // the auth0 domain prefix
+    url: 'ezufsnd.us', // the auth0 domain prefix
     audience: 'http://127.0.0.1:5000', // the audience set for the auth0 app
     clientId: 'RuBUGVhuWSJL7dBoWekcOeOf462x3NL7', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
