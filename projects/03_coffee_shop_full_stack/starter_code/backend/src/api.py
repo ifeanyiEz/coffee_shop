89138c1dbb60cf7a2e91a920e58bc887c721a6d5
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2022-06-05 00:36:06 +0100

    Modify code, add comments

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+from flask import session, redirect, url_for
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
@@ -18,6 +19,7 @@ from .auth.auth import AuthError, requires_auth
 # https://www.programiz.com/python-programming/list
 # https://www.programiz.com/python-programming/methods/built-in/list
 # https://stackoverflow.com/questions/12952546/sqlite3-interfaceerror-error-binding-parameter-1-probably-unsupported-type
+# https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_orm_updating_objects.htm
 
 
 #_____________________INITIALIZE THE APP______________________#
@@ -38,6 +40,8 @@ DONE uncomment the following line to initialize the datbase
 
 
 #____________________DEFINE ENDPOINTS__________________________#
+
+#__________________Get All Drinks: Public___________________#
 '''
 @DONE implement endpoint
     GET /drinks
@@ -53,7 +57,7 @@ def get_drinks():
         all_drinks = Drink.query.all()
         formatted_drinks = [drink.short() for drink in all_drinks]
 
-        # Confirm that there are drinks in the DB, otherwise show a "not found error message".
+        # Confirm that there are drinks in the DB, otherwise show a "404: not found" error message.
         if len(formatted_drinks) == 0:
             abort(404)
 
@@ -66,6 +70,9 @@ def get_drinks():
     except:
         abort(422)
 
+
+#__________________Get All Drinks: Authorised Users___________________#
+
 '''
 @DONE implement endpoint
     GET /drinks-detail
@@ -75,6 +82,7 @@ def get_drinks():
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks-detail', methods=['GET'])
+
 # Ensure that the user has the permissions to access this endpoint
 @requires_auth('get:drinks-detail')
 def get_drinks_detail(payload):
@@ -85,7 +93,7 @@ def get_drinks_detail(payload):
         all_drinks = Drink.query.all()
         formatted_drinks = [drink.long() for drink in all_drinks]
 
-        # Confirm that there are drinks in the DB, otherwise show a "not found error message".
+        # Confirm that there are drinks in the DB, otherwise show a "404: not found" error message.
         if len(formatted_drinks) == 0:
             abort(404)
 
@@ -98,22 +106,32 @@ def get_drinks_detail(payload):
     except:
         abort(422)
 
+#__________________Get Specific Drink: Authorised Users___________________#
+
 '''
 This is a new endpoint.
 It is added to allow users with proper permissions to retrieve and view details of a specific drink, then decide how/what to PATCH.
-Also it allows permitted users to view a drink before deciding to DELETE
+Also it allows permitted users to view a drink before deciding to DELETE it.
 
 '''
 @app.route('/drinks/<int:drink_id>', methods=['GET'])
+
+# Ensure that the user has the permissions to access this endpoint
 @requires_auth('get:drinks')
 def get_drink_by_id(payload, drink_id):
+
     try:
+        # From the DB, fetch the drink using the drink_id
         drink = Drink.query.filter_by(id = drink_id).one_or_none()
+
+        # If there's no drink with the specified drink_id, raise a "404: not found" error
         if drink is None:
             abort(404)
         
+        # If the drink is found present it in the long() format 
         formatted_drink = drink.long()
 
+        # Return success and the formatted drink
         return jsonify({
             "success": True,
             "drink": formatted_drink
@@ -121,7 +139,9 @@ def get_drink_by_id(payload, drink_id):
     except:
         abort(422)
 
-        
+
+#__________________Make New Drink: Authorised Users___________________#
+
 '''
 @DONE implement endpoint
     POST /drinks
@@ -132,33 +152,41 @@ def get_drink_by_id(payload, drink_id):
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks', methods=['POST'])
+
 # Ensure that the user has the permissions to access this endpoint
 @requires_auth('post:drinks')
 def make_new_drink(payload):
 
+    # Get the data from the request body
     data = request.get_json()
     title = data.get('title', None)
     recipe = data.get('recipe', None)
 
     try:
+        # Make a new drink using the data from the request body
         new_drink = Drink(title = title, recipe = json.dumps(recipe))
 
+        # If any of the request fields is blank, raise a "400: bad syntax" error.
         if new_drink.title == "" or new_drink.recipe == "":
             return jsonify({
                 "success": False,
                 "message": 'The server could not understand the request due to invalid syntax'
             }), 400
 
+        # If everything is okay, insert the new drink to the DB
         else:
             new_drink.insert()
 
+        # Return success and the new drink in  the long() format
         return jsonify({
             "success": True,
             "drinks": new_drink.long()
-        })
+        }), 200
 
     except:
         abort(422)
+
+#__________________Update Specific Drink: Authorised Users___________________#
 
 '''
 @DONE implement endpoint
@@ -172,40 +200,65 @@ def make_new_drink(payload):
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+
 # Ensure that the user has the permissions to access this endpoint
 @requires_auth('patch:drinks')
 def update_drink(payload, drink_id):
 
+    # Get the data from the request body
+    data = request.get_json()
+    title = data.get('title', None)
+    recipe = data.get('recipe', None)
+
     try:
+        # From the DB, fetch the drink to be updated using the drink_id
         drink = Drink.query.filter_by(id = drink_id).one_or_none()
 
+        # If there's no drink with the specified drink_id, raise a "404: not found" error
         if drink is None:
             abort(404)
 
-        data = request.get_json()
-        title = data.get('title', None)
-        recipe = data.get('recipe', None)
+        # If the drink is found check:
+        # If only the title is to be changed, assign a new value to the drink title
+        if recipe is None and title is not None:
+            drink.title = title
 
-        drink.title = title
-        drink.recipe = json.dumps(recipe)
+            # Update the DB
+            drink.update()
 
-        if drink.title == "" and drink.recipe == "":
+        # If only the recipe is to be changed, assign a new value to the drink recipe
+        elif title is None and recipe is not None:
+            drink.recipe = json.dumps(recipe)
+
+            # Update the DB
+            drink.update()
+
+        # If no values are provided for both title and recipe, raise a "400: bad syntax" error.
+        elif drink.title is None and drink.recipe is None:
             return jsonify({
                 "success": False,
-                "message": 'The server could not understand the request due to invalid syntax'
+                "message": 'The server could not understand the request, no values were provided for title or recipe'
             }), 400
 
-        drink.update()
+        # If both values are provided, assign new values for both title and recipe
+        else:
+            drink.title = title
+            drink.recipe = json.dumps(recipe)
+            drink.update()
 
+        # Get the new updated drink from the DB
         updated_drink = Drink.query.filter_by(id = drink_id).one_or_none()
 
+        # Present the required JSON success output
         return jsonify({
             "success": True,
-            "drinks": updated_drink.long()
-        })
+            "drinks": [updated_drink.long()]
+        }), 200
 
     except:
         abort(422)
+
+#__________________Delete Specific Drink: Authorised Users___________________#
 
 '''
 @DONE implement endpoint
@@ -218,29 +271,49 @@ def update_drink(payload, drink_id):
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+
 # Ensure that the user has the permissions to access this endpoint
 @requires_auth('delete:drinks')
-
 def delete_drink(payload, drink_id):
 
     try:
+        # From the DB, fetch the drink to be deleted using the drink_id
         drink = Drink.query.filter_by(id = drink_id).one_or_none()
 
+        # If there's no drink with the specified drink_id, raise a "404: not found" error
         if drink is None:
             abort(404)
 
+        # If the drink is found, delete it from the DB
         drink.delete()
 
-        deleted_drink_id = drink.id
+        # Confirm that the drink was deleted by searching for it in the DB
+        deleted_drink = Drink.query.filter_by(id = drink_id).one_or_none()
 
-        return jsonify({
-            "success": True,
-            "delete": deleted_drink_id
-        })
+        # If it is found, then it was not deleted. Raise a "422: unable to be followed" error.
+        if deleted_drink is not None:
+            return jsonify({
+                "success": False,
+                "message": "Drink was not deleted"
+            }), 422
+        
+        else:
+            # If it is not found, then it was deleted. Return success and the drink id
+            deleted_drink_id = drink.id
+
+            return jsonify({
+                "success": True,
+                "delete": deleted_drink_id
+            }), 200
 
     except:
         abort(422)
 
+
+@app.route('/logout')
+def logout_user():
+    session.clear()
+    return redirect(url_for('get_drinks'))
 
 
 #__________________HANDLING ERRORS________________________#
